863edd457114cefbd9b738136be8e8709f0cc154
Quyến Black <havanan.8ph@gmail.com>
2026-04-22 10:11:30 +0700

    GameSoNgauNhien
    Mình sẽ đoán một số nó radom nếu đúng thì win và sai thì lose

---
+++ DoanSoNgauNhien/src/GameDoanSoNgauNhien/Text.java
@@ -0,0 +1,13 @@
+package GameDoanSoNgauNhien;
+
+public class Text {
+
+	public static void main(String[] args) {
+		String s="Xin chào Obama! Tui là Putin";
+		String s3=s.substring(9, 14);
+		System.out.println(s3);
+		// TODO Auto-generated method stub
+
+	}
+
+}
