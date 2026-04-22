ebd326bdced7adeb5930085e3286138717a2b121
Quyến Black <havanan.8ph@gmail.com>
2026-04-22 10:29:49 +0700

    TachChuoi
    Có 1 link nhạc và cách nó theo chuổi

---
+++ BaiTachDuongDan/src/TachLink/LinkNhac.java
@@ -0,0 +1,29 @@
+package TachLink;
+import java.util.*;
+public class LinkNhac 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s = "D:/music/bolero/longme.mp3";
+		String s1 = s.substring(16);
+		String s2 = s.substring(16,22);
+		try
+		{	
+			
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			System.out.println(s1);
+			System.out.println(s2);
+		}
+
+
+	}
+
+}
