ef57c101f4c9897196ed8f3442dee44d35d757d0
Quyến Black <havanan.8ph@gmail.com>
2026-05-27 10:47:26 +0700

    QuanLyGiaoVienBai1
    Cho 2 bảng bên MySQL gồm khoa và giáo  viên .sau đó qua java tạo các package data, domain  và test . Sau đó bên hàm main em cho 1 cái menu để nhập thêm xóa sữa dữ liêu bên data .

---
+++ BaiThucHanh/src/test/java/data/JavaConnection.java
@@ -0,0 +1,22 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JavaConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/quanlygiaoduc";
+    private static final String USER = "root";
+    private static final String PASSWORD = "230426Qu"; 
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("Loi ket noi database!");
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
